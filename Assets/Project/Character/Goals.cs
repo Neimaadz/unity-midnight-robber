80f06896152b1bf05c64547e0785c5dae7752685
Nathan <l.nathan97460@gmail.com>
2022-07-01 13:40:29 +0200

    Utilisation d'un asset pour la porte et gestion de la couleur de la lumière

--- Assets/Project/Character/Goals.cs
+++ Assets/Project/Character/Goals.cs
@@ -1,41 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
-public class Goals : MonoBehaviour
-{
-    private int count;
+public class Goals: MonoBehaviour {
+  [SerializeField]
+  public Light light;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+  private int count;
+
+  // Start is called before the first frame update
+  void Start() {}
+
+  // Update is called once per frame
+  void Update() {
+    if (count == 3) {
+      StaticTimer.objectsFound = true;
+      light.color = Color.green;
     }
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (count == 3)
-        {
-            StaticTimer.objectsFound = true;
-        }
+  private void OnCollisionEnter(Collision collision) {
+    if (collision.gameObject.name.Equals("dollars")) {
+      Destroy(collision.gameObject);
+      count++;
+    } else if (collision.gameObject.name.Equals("smartphone")) {
+      Destroy(collision.gameObject);
+      count++;
+    } else if (collision.gameObject.name.Equals("paint_sheep")) {
+      Destroy(collision.gameObject);
+      count++;
     }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name.Equals("dollars"))
-        {
-            Destroy(collision.gameObject);
-            count++;
-        }
-        else if (collision.gameObject.name.Equals("smartphone"))
-        {
-            Destroy(collision.gameObject);
-            count++;
-        }
-        else if (collision.gameObject.name.Equals("paint_sheep"))
-        {
-            Destroy(collision.gameObject);
-            count++;
-        }
-    }
+  }
 }
